0ec480652c40527ad6e99d0b366e11acb6fc8cea
Victors1967 <victorsmirnov@me.com>
2021-09-27 17:58:05 +0300

    projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { projects } from 'src/app/models/projects';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  skills = ['all', 'javascript', 'ui', 'backend', 'frontend'];
+
+  id: string | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public projects: any = projects;
+
+   // filter button
+  filterBtn(event: any) {
+    const projectCards = projects;
+    projectCards.forEach(card => {
+      if (card.tags.includes(event.target.attributes.id.value)) {
+        card.display = 'hide';
+      } else {
+        card.display = '';
+      }
+    });
+    
+    for (let e of event.target.parentElement.children) {
+      e.attributes.class.value = 'filter-btn ng-star-inserted';
+    }
+    event.target.attributes.class.value += ' active';
+  }
+
 
 }
---
+++ src/app/models/projects.ts
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #ui/ux, #backend",
+        image: "project1.png",
+        display: ""
+    },
+    {
+        name: "project two",
+        tags: "#javascript, #fullstack",
+        image: "project2.png",
+        display: ""
+    },
+    {
+        name: "project three",
+        tags: "#java, #mongodb, #angular, #fullstack",
+        image: "project3.png",
+        display: ""
+    },
+]
