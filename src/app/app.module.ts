d17a8c72f36339d32cc91aa51a2b21419e1250b0
Victors1967 <victorsmirnov@me.com>
2021-09-27 13:12:00 +0300

    3333

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { MenuListItemComponent } from './components/menu-list-item/menu-list-ite
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialUiModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
