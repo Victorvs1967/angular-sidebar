94660a9406393a117a9841d64c503fe5cf1c883d
Victors1967 <victorsmirnov@me.com>
2021-09-27 08:59:03 +0300

    2222

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { SideMainNavComponent } from './components/side-main-nav/side-main-nav.c
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
-  {
-    path: 'admin',
+  { path: 'admin',
     children: [
       { path: '', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
     ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +19,6 @@ import { MenuListItemComponent } from './components/menu-list-item/menu-list-ite
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialUiModule,
-    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
